8c57055074b2131ccd73d3fd369dbef95b1cd2f3
0x6767 <gilates6@gmail.com>
2026-01-25 13:12:37 +0700

    add remotes for game item interactions

---
+++ libraries/bedfight/remotes.lua
@@ -0,0 +1,7 @@
+return {
+    SwordHitRemote = game:GetService('ReplicatedStorage').Remotes.ItemsRemotes.SwordHit,
+    MineBlockRemote = game:GetService('ReplicatedStorage').Remotes.ItemsRemotes.MineBlock,
+    EquipRemote = game:GetService('ReplicatedStorage').Remotes.ItemsRemotes.EquipTool,
+    EquipCape = game:GetService('ReplicatedStorage').Remotes.ItemsRemotes.EquipCape,
+    TakeItemFromChest = game:GetService('ReplicatedStorage').Remotes.ItemsRemotes.TakeItemFromChest
+}
